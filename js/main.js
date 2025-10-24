459ca5a7089946fe54bd562c7d431a7cac0f6b09
margaretjou <97330715+margaretjou@users.noreply.github.com>
2025-10-23 23:42:14 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -18,3 +18,41 @@ function responsive_control() {
     x.className = "topnav";
   }
 }
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "More Info";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Less Info";
+    moreText.style.display = "inline";
+  }
+}
+
+
+// Get the button:
+let mybutton = document.getElementById("topButton");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
